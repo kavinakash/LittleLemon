84070e3195b5e33dba8de593e966770c57bb0d93
KAVIN AKASH <kavinakash06@gmail.com>
2023-12-26 12:13:33 +0530

    meta tags
    meta tags added to header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <>
+    <body>
       <Header />
       <Main />
       <Footer />
-    </>
+    </body>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import Nav from "./Nav.js"
-
+import React from 'react';
+import Nav from './Nav.js';
 
 function Header() {
   return (
     <header>
-          <img src="little_lemon\public\images\logo.png" alt="logo"/>
+      <head>
+          <meta name="description" content='Little lemon restaurant - food'/>
+          <meta name="og:title" content="Little Lemon "/>
+          <meta name="og:description" content="Little lemon located in chicago"/>
+          <meta name="og:image" content="little_lemon\public\images\logo.png"/>
+      </head>
+          <img src='little_lemon\public\images\logo.png' alt="logo"/>
           <Nav />
     </header>
   )
